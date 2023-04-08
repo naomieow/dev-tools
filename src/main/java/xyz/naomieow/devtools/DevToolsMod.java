60ee2d8836d6e9bcc3121ebd5e1ee2fc85936307
IzukiKa <isaacjroberts0505@gmail.com>
2023-04-08 16:38:28 +0100

    Update DevToolsMod.java

--- src/main/java/xyz/naomieow/devtools/DevToolsMod.java
+++ src/main/java/xyz/naomieow/devtools/DevToolsMod.java
@@ -4,7 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+
 public class DevToolsMod implements ModInitializer {
+	public static Path worldSaveLocation;
+	public static Path worldDatapackLocation;
 	public static final Logger LOGGER = LoggerFactory.getLogger("devtools");
 
 	@Override
